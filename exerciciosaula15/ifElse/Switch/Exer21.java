33d1452152b503823d3e0c2754768e44fe2c2e6a
Emanuellysac <36846599+Emanuellysac@users.noreply.github.com>
2018-03-14 01:53:15 -0300

    Update Exer21.java

--- exerciciosaula15/ifElse/Switch/Exer21.java
+++ exerciciosaula15/ifElse/Switch/Exer21.java
@@ -1,4 +1,4 @@
-/* Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+ /* Um posto está vendendo combustíveis com a seguinte tabela de descontos:
 . Álcool:
 a. até 20 litros, desconto de 3% por litro
 b. acima de 20 litros, desconto de 5% por litro
@@ -6,7 +6,7 @@ Gasolina:
 c. até 20 litros, desconto de 4% por litro
 d. acima de 20 litros, desconto de 6% por litro
 Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, Ggasolina),
-calcule e imprima o valor a ser pago pelo cliente. */
+calcule e imprima o valor a ser pago pelo cliente. sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90. */
 
 
 package br.com.exerciciosaula15.ifElse.Switch;
